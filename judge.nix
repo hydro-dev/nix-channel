6fd23aa2e7a8628701ed53a421f62d6ea256a3fe
undefined <i@undefined.moe>
2022-08-09 03:00:53 +0800

    judge packages

--- judge.nix
+++ judge.nix
@@ -1,14 +1,35 @@
-{ pkgs ? import <nixpkgs> { system = "x86_64-linux"; } }:
+# Note: this file is used to construct HydroOJ judge rootfs.
+{ 
+  pkgs ? import <nixpkgs> { system = "x86_64-linux"; },
+  minimal ? false
+}:
 
 pkgs.buildEnv {
-  name = "judge";
+  name = "judge${if minimal then "-minimal" else ""}";
   paths = [
     pkgs.busybox
-    pkgs.gcc_latest
-    pkgs.fpc
+    pkgs.bash
     pkgs.diffutils
     pkgs.unzip
-  ];
+    pkgs.gcc
+    pkgs.fpc
+  ] ++ (if !minimal then [
+    pkgs.ghc
+    pkgs.rustc
+    pkgs.python2
+    pkgs.pythonPackages.numpy
+    pkgs.python3Minimal
+    pkgs.python3Packages.numpy
+    pkgs.php
+    pkgs.go
+    pkgs.nodejs
+    pkgs.esbuild
+    pkgs.openjdk_headless
+    pkgs.ruby
+    pkgs.mono
+    pkgs.julia_17-bin
+    pkgs.verilog
+  ] else []);
   ignoreCollisions = true;
   pathsToLink = [ "/bin" ];
 }
