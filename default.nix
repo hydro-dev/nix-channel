c39d016b6565ee09de31306167f2f7a35a43dd0a
undefined <i@undefined.moe>
2022-08-08 21:47:52 +0800

    1

--- default.nix
+++ default.nix
@@ -10,7 +10,6 @@ let
     mongodb6 = callPackage ./mongodb.nix { version = "6.0.0"; };
     mongodb5 = callPackage ./mongodb.nix { version = "5.0.10"; };
     mongodb4 = callPackage ./mongodb.nix { version = "4.4.15"; };
-    # mongosh6 = callPackage ./mongodb.nix { version = "6.0.0"; type = "shell"; };
     mongosh5 = callPackage ./mongodb.nix { version = "5.0.10"; type = "shell"; };
     mongosh4 = callPackage ./mongodb.nix { version = "4.4.15"; type = "shell"; };
     judge = callPackage ./judge.nix {};
