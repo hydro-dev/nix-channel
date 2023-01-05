1710306cb2bf1af3ebf1452829c5751bb6af028d
undefined <i@undefined.moe>
2023-01-06 01:04:10 +0800

    sandbox: update to 1.6.2

--- sandbox.nix
+++ sandbox.nix
@@ -1,18 +1,16 @@
 { 
   system ? builtins.currentSystem,
   pkgs ? import <nixpkgs> { system = system; },
-  version ? "1.5.4"
+  version ? "1.6.2"
 }:
 
 with pkgs.lib;
 let
   sha256dict = {
-    "1.5.1x86_64-linux" = "sha256-bJ8dOpiIDM+Iubd8IBAkSPsHpJSUdOsiUozuHGHMVyE=";
-    "1.5.2x86_64-linux" = "sha256-ABtoJ3ABQl2Ymg5aowB+lkrWYLQKsbgKBFLMzRsD3HY=";
-    "1.5.3x86_64-linux" = "sha256-aSL972fMHhR5LZe7HK2VxAur0sWXjfjJY2L2PxR95Po=";
-    "1.5.3aarch64-linux" = "sha256-33iXaD2niZaaT7YoIi5XpIjOipk37lfoD6NM6+OICps=";
     "1.5.4x86_64-linux" = "sha256-fpSRP3jQCWebAsEa1K/71Hw+q6pwOfLcrO5f29sa8qk=";
     "1.5.4aarch64-linux" = "sha256-2JZp69BLAwWUJp3EHtAKmuzpt+XsjokSQN/+faTvXCI=";
+    "1.6.2x86_64-linux" = "sha256:1mlxxx27zax0r0ikny0sjk85a6mbkhaz2b11670sjckp9j659pvs";
+    "1.6.2aarch64-linux" = "sha256:0wlf8q93hbmn3qyg577v2r9vbvyqvlsk36r10mwxyddf784yb3kg";
   };
   systemMap = {
     "x86_64-linux" = "linux_amd64";
