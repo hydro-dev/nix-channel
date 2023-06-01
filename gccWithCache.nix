69b4353cfed4864bc5216ce9c1533957a62b087f
undefined <i@undefined.moe>
2023-06-01 10:15:39 +0800

    fix gcc cache

--- gccWithCache.nix
+++ gccWithCache.nix
@@ -1,12 +1,16 @@
-{ 
-  system ? builtins.currentSystem,
-  pkgs ? import <nixpkgs> { system = system; },
-  gccUnwrapped ? pkgs.gcc-unwrapped
+{ system ? builtins.currentSystem
+, pkgs ? import <nixpkgs> { system = system; }
+, gccUnwrapped ? pkgs.gcc-unwrapped
 }:
 
 let
-  gccPatched = pkgs.runCommand "gcc" {} ''
+  gccPatched = pkgs.runCommand "gcc"
+    {
+      outputs = [ "out" "lib" "libgcc" ];
+    } ''
     cp -r ${gccUnwrapped} $out
+    cp -a ${gccUnwrapped.lib} $lib
+    cp -a ${gccUnwrapped.libgcc} $libgcc
     function gchgen {
       echo $2 $3
       ${gccUnwrapped}/bin/g++ -x c++-header -lm -fno-stack-limit -fdiagnostics-color=always -std=$2 -c $1 -o $1.gch/$2.gch $3
@@ -40,4 +44,5 @@ let
     ensureCache iostream
     ensureCache cstdio
   '';
-in pkgs.wrapCC gccPatched
+in
+pkgs.wrapCC gccPatched
